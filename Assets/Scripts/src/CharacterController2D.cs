1bce548abb020197c3e9cc965c5f01a685cc0208
hangarter <madison.paz@gmail.com>
2020-04-26 17:07:30 +0200

    Using triggers instead of normal colliders

--- Assets/Scripts/src/CharacterController2D.cs
+++ Assets/Scripts/src/CharacterController2D.cs
@@ -11,6 +11,7 @@ public class CharacterController2D : MonoBehaviour
     [SerializeField] private float gravity;
 
     private Rigidbody2D _rigidbody2D;
+    private Collider2D _collider2D;
 
 #if UNITY_EDITOR
     public Rigidbody2D Rigidbody2D
@@ -22,16 +23,21 @@ public class CharacterController2D : MonoBehaviour
     
     private void Start()
     {
+        _collider2D = gameObject.GetComponent<Collider2D>();
         _rigidbody2D = gameObject.GetComponent<Rigidbody2D>();
+        _rigidbody2D.isKinematic = true;
+        _collider2D.isTrigger = true;
     }
 
     public CollisionFlags Move(Vector2 moveDirection)
     {
-        // gameObject.transform.Translate(new Vector3(moveDirection.x, moveDirection.y));
-        var position = gameObject.transform.position + new Vector3(moveDirection.x, moveDirection.y);
-        _rigidbody2D.MovePosition(position);
+        gameObject.transform.Translate(new Vector3(moveDirection.x, moveDirection.y));
         return CollisionFlags.None;
     }
 
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log(other);
+    }
 
 }
