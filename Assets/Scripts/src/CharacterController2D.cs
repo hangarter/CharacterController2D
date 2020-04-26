c407cd9e3d5356823198c258fd16a2d20c89ce84
hangarter <madison.paz@gmail.com>
2020-04-26 15:41:57 +0200

    Initial changes released to public

---
+++ Assets/Scripts/src/CharacterController2D.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider2D))]
+public class CharacterController2D : MonoBehaviour
+{
+    [SerializeField] private float gravity;
+
+    public CollisionFlags Move(Vector2 moveDirection)
+    {
+        gameObject.transform.position += new Vector3(moveDirection.x, moveDirection.y);
+        return CollisionFlags.None;
+    }
+}
---
+++ Assets/Scripts/test/CharacterController2DTest.cs
@@ -0,0 +1,77 @@
+﻿using NUnit.Framework;
+using UnityEngine;
+
+namespace test
+{
+    [TestFixture]
+    public class CharacterController2DTest
+    {
+        private GameObject _gameObject;
+        
+        [SetUp]
+        public void SetUp()
+        {
+            _gameObject = new GameObject();
+        }
+        
+        [Test]
+        public void Move_ReceivesVectorUp_MovesOneUnitUp()
+        {
+            // Given
+            var characterController2D = _gameObject.AddComponent<CharacterController2D>();
+            var moveDirection = Vector2.up;
+            _gameObject.transform.position = new Vector3(2,3);
+            
+            // When
+            characterController2D.Move(moveDirection);
+            
+            // Then
+            Assert.AreEqual(new Vector3(2,4), _gameObject.transform.position);
+        }
+        
+        [Test]
+        public void Move_ReceivesVectorDown_MovesOneUnitDown()
+        {
+            // Given
+            var characterController2D = _gameObject.AddComponent<CharacterController2D>();
+            var moveDirection = Vector2.down;
+            _gameObject.transform.position = new Vector3(2,3);
+            
+            // When
+            characterController2D.Move(moveDirection);
+            
+            // Then
+            Assert.AreEqual(new Vector3(2,2), _gameObject.transform.position);
+        }
+        
+        [Test]
+        public void Move_ReceivesVectorLeft_MovesOneUnitLeft()
+        {
+            // Given
+            var characterController2D = _gameObject.AddComponent<CharacterController2D>();
+            var moveDirection = Vector2.left;
+            _gameObject.transform.position = new Vector3(2,3);
+            
+            // When
+            characterController2D.Move(moveDirection);
+            
+            // Then
+            Assert.AreEqual(new Vector3(1,3), _gameObject.transform.position);
+        }
+        
+        [Test]
+        public void Move_ReceivesVectorRight_MovesOneUnitRight()
+        {
+            // Given
+            var characterController2D = _gameObject.AddComponent<CharacterController2D>();
+            var moveDirection = Vector2.right;
+            _gameObject.transform.position = new Vector3(2,3);
+            
+            // When
+            characterController2D.Move(moveDirection);
+            
+            // Then
+            Assert.AreEqual(new Vector3(3,3), _gameObject.transform.position);
+        }
+    }
+}
